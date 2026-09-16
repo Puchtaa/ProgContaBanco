6bd9bf417d461c2939ce89e4ff53a0392601180d
Puchtaa <rafael.puchta@aluno.senai.br>
2026-09-15 21:11:46 -0300

    Adicionadas funções para não deixar sacar valores maiores que o saldo atual e também não deixar sacar valores negativos!

--- src/Main.java
+++ src/Main.java
@@ -8,30 +8,50 @@ public class Main {
         int opcao = 0;
 
         while (opcao != 9) {
-            System.out.println("\nSaldo atual: R$ %.2f " + saldo);
+
+            System.out.printf("Saldo atual: R$ %.2f\n", saldo);
             System.out.println("--- MENU ---");
+            System.out.println("Escolha uma opção: ");
             System.out.println("1: Depositar");
             System.out.println("2: Sacar");
             System.out.println("9: Sair");
-            System.out.print("Escolha uma opção: ");
+
             opcao = leitor.nextInt();
 
             if (opcao == 1) {
                 System.out.print("Digite o valor para depositar: ");
                 double valor = leitor.nextDouble();
-                saldo += valor;
+
+
+                if (valor > 0) {
+                    saldo += valor;
+                    System.out.println("Depósito realizado com sucesso!");
+                } else {
+                    System.out.println("O valor do depósito deve ser maior que zero.");
+                }
+
 
             } else if (opcao == 2) {
                 System.out.print("Digite o valor para sacar: ");
                 double valor = leitor.nextDouble();
-                saldo -= valor;
+
+
+                if (valor <= 0) {
+                    System.out.println("O valor do saque deve ser maior que zero.");
+                } else if (valor > saldo) {
+                    System.out.printf("Saldo insuficiente! Seu saldo atual é de R$ %.2f.\n", saldo);
+                } else {
+                    saldo -= valor;
+                    System.out.println("Saque realizado com sucesso!");
+                }
+
 
             } else if (opcao != 9) {
                 System.out.println("Opção inválida!");
             }
         }
 
-        System.out.println("Programa encerrado. Até logo!");
+        System.out.println("Seção encerrada. Até logo!");
         leitor.close();
     }
 }
